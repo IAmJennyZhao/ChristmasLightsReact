ab86e569027d946ac6b60cdd93d18bb3553759cb
Jenny Zhao <jennywuzhao@gmail.com>
2024-01-21 02:16:40 -0500

    Implement theme changing function

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ import Select from '@mui/material/Select';
 function App() {
   const [colors, setColor] = useState(['#ffadad', '#ffd6a5', '#fdffb6', '#caffbf', '#9bf6ff', '#a0c4ff', '#bdb2ff', '#ffc6ff', '#ffadad', '#ffd6a5', '#fdffb6', '#caffbf', '#9bf6ff', '#a0c4ff', '#bdb2ff', '#ffc6ff']);
 
-  // speed domain 0-100 => updateTime range 1000-100
+  // speed domain 0-100 => updateTime range 500-50
   const [speed, setSpeed] = useState(30);
   const handleSpeedChange = (event, newSpeed) => {
     setSpeed(newSpeed);
@@ -25,6 +25,32 @@ function App() {
   const [theme, setTheme] = useState('');
   const handleThemeChange = (event) => {
     setTheme(event.target.value);
+    switch (event.target.value) {
+      case 0: 
+        // Rainbow
+        setColor(['#ffadad', '#ffd6a5', '#fdffb6', '#caffbf', '#9bf6ff', '#a0c4ff', '#bdb2ff', '#ffc6ff', '#ffadad', '#ffd6a5', '#fdffb6', '#caffbf', '#9bf6ff', '#a0c4ff', '#bdb2ff', '#ffc6ff']);
+        break;
+      case 1:
+        // Christmas
+        setColor(['#ffffff', '#ff7878', '#ff0000', '#ff7878', '#ffffff', '#74d680', '#378b29', '#74d680', '#ffffff', '#ff7878', '#ff0000', '#ff7878', '#ffffff', '#74d680', '#378b29', '#74d680']);
+        break;
+      case 2: 
+        // Cool Neon
+        setColor(['#9900ff', '#9966ff', '#9999ff', '#99ccff', '#99ffff', '#99ccff', '#9999ff', '#9966ff', '#9900ff', '#9966ff', '#9999ff', '#99ccff', '#99ffff', '#99ccff', '#9999ff', '#9966ff']);
+        break;
+      case 3: 
+        // Warm Neon
+        setColor(['#ff0033', '#ff6633', '#ff9933', '#ffcc33', '#ffff33', '#ffcc33', '#ff9933', '#ff6633', '#ff0033', '#ff6633', '#ff9933', '#ffcc33', '#ffff33', '#ffcc33', '#ff9933', '#ff6633']);
+        break;
+      case 4:
+        // Purple
+        setColor(['#eebed5', '#d6a0bf', '#b97ea6', '#985b87', '#69224e', '#985b87', '#b97ea6', '#d6a0bf', '#eebed5', '#d6a0bf', '#b97ea6', '#985b87', '#69224e', '#985b87', '#b97ea6', '#d6a0bf']);
+        break;
+      default: 
+        // White
+        setColor(['#e4e4e4', '#c7c7c7', '#b7b7b7', '#a4a4a4', '#909090', '#a4a4a4', '#b7b7b7', '#c7c7c7', '#e4e4e4', '#c7c7c7', '#b7b7b7', '#a4a4a4', '#909090', '#a4a4a4', '#b7b7b7', '#c7c7c7']);
+        break;
+    }
   };
 
   useEffect(() => {
@@ -35,7 +61,7 @@ function App() {
       setColor(newColors);
 
       // interval of change
-    }, 1000 - 9 * speed);
+    }, 500 - 4.5 * speed);
 
     return () => clearInterval(interval);
   }, [speed, colors]);
@@ -43,14 +69,14 @@ function App() {
   var lights = colors.slice(0, 9).map((e, i) => <Light fill={e} key={`light${i}`} className='lightbulb' />)
 
   return (
-    <div className="App">      
+    <div className="App">
       <div>
         {lights}
       </div>
       <div>
         {lights}
       </div>
-      
+
       <div className="Settings">
         <SpeedControls speed={speed} handleSpeedChange={handleSpeedChange} />
         <ThemeSettings theme={theme} handleThemeChange={handleThemeChange} />
@@ -77,7 +103,7 @@ function SpeedControls({ speed, handleSpeedChange }) {
 
 function ThemeSettings({ theme, handleThemeChange }) {
 
-  const themeTexts = ["Rainbow 1", "Christmas", "White", "Red", "Teal", "Purple"];
+  const themeTexts = ["Rainbow", "Christmas", "Cool Neon", "Warm Neon", "Purple", "White"];
   const themeMenuItems = themeTexts.map((e, i) => <MenuItem value={i} className="ThemeText">{e}</MenuItem>)
 
   return (
